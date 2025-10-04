7f4e65b1b92bb311f7cd1ba4869e0f2f16b95184
VizalMurugesan <vizalrmurugesan@gmail.com>
2025-10-04 15:52:31 -0700

    dd

--- Assets/WorldMap.cs
+++ Assets/WorldMap.cs
@@ -5,6 +5,17 @@ public class WorldMap : MonoBehaviour, IPointerClickHandler
 {
     void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        if(eventData.button == PointerEventData.InputButton.Left)
+        {
+            Debug.Log("Clicked at: " + eventData.position);
+            GameManager.Instance.CheckForArea((Vector2)eventData.position);
+            Debug.Log("clicked");
+        }
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            Debug.Log("Clicked at: " + eventData.position);
+            GameManager.Instance.CheckForArea((Vector2)eventData.position);
+            Debug.Log("clicked");
+        }
     }
 }
